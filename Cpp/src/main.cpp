06c0889d7d4b85f128d2ab5591075c983eee1c70
TolikRogov <tolik_rogov@bk.ru>
2026-02-17 14:11:52 +0300

    delete .env file; change main to cpp code; rework cmake and make

--- Cpp/src/main.cpp
+++ Cpp/src/main.cpp
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <iostream>
 
 int main(int argc, char **argv) {
 
 	if (argc > 1)
-		printf("%s\n", argv[1]);
+		std::cout << argv[1] << std::endl;
 	else
-		printf("Hello, world!\n");
+		std::cout << "Hello, world!" << std::endl;
 
 	return 0;
 }
